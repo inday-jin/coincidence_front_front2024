feeeb9ce5ba356190e3a576d1cdeda0c83afb743
jin <jin@inday.co.kr>
2024-12-30 15:21:40 +0900

    가입안내_기간제 수정

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -62,7 +62,7 @@ export default function Products(){
     <>
       <div className="inday_container pb120">
         <h2 className="main_tit fz50 ffsd6 pb40 pt120 tac" ref={(el) => (elementsRef.current[0] = el)}><span className="mark_bg">우연 기간제 멤버십</span></h2>
-        <p className="tac fz22 c3 pb80" ref={(el) => (elementsRef.current[1] = el)}>기간제 서비스는 <span className="c1 ffsd6">기간 내 횟수 제한 없이 만남이 가능</span>한서비스 입니다.<br/>계약 기간 동안 매니저님과 원활히 소통하여 평생 인연 찾으시기 바랍니다.<br/>*우연은 공정거래위원회 국내 결혼 표준 약관을 100% 준수합니다.<br/>우연은 공정 거래위원회 국내 결혼 표준 약관을 준수하며, 환불 약정 없이 이용 부금만 차감 후 전액 환불을 약속합니다.</p>
+        <p className="tac fz22 c3 pb80" ref={(el) => (elementsRef.current[1] = el)}>기간제 서비스는 <span className="c1 ffsd6">기간 내 횟수 제한 없이 만남이 가능</span>한서비스 입니다.<br/>계약 기간 동안 매니저님과 원활히 소통하여 평생 인연 찾으시기 바랍니다.<br/>* 우연은 공정 거래위원회 국내 결혼 표준 약관을 준수하며, 부당한 환불 약정 없이 이용 금액 만큼만 차감 후 환불을 약속합니다.</p>
         <div className="product_lst subscribe">
           {prodData[0].map((item, i) => {
             const match = item.desc.match(/\[(.*?)\]/);
@@ -88,7 +88,7 @@ export default function Products(){
                   {item.salePrice !== 0 ?
                     <div className="sale_price pb10">
                       <p className="price tac fz20 c9">{String(item.salePrice).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} <span className="fz20 ffsd4">원</span></p>
-                      <span className="sale_percent fz24 ffsd6 cf">{otherContent}</span>
+                      <span className="sale_percent fz24 ffsd6 cf">{otherContent} OFF</span>
                     </div>
                   :
                     <div className="sale_price pb10 none">
@@ -97,12 +97,17 @@ export default function Products(){
                   }
                   <p className="price tac fz30 ffsd6 pb10">{String(item.price).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} <span className="fz20 ffsd4">원</span></p>
                   <p className="vat tac fz16 c6 pb35">VAT 포함</p>
-                  {/* <p className="desc tac c1 fz18">{otherContent}</p> */}
+                  <div className="bottom_txt_box tac">
+                    <p className="p1 fz18">당일 결제 혜택</p>
+                    <p className="p2 fz24 ffsd6"><span className="sameday_per">{item.samedayPer}%</span> 추가 할인</p>
+                    <p className="p3 fz18 c9">(혜택 적용 금액 {String(item.samedayPrice).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원)</p>
+                  </div>
                 </div>
               </div>
             )
           })}
         </div>
+        <p className="pt30 tac fz16 c3" ref={(el) => (elementsRef.current[13] = el)}>우연은 정찰제로 운영되며 타사와 다르게 상담 후 가격이 변동되지 않습니다.</p>
       </div>
       <div className="membership_lst">
         <div className="inday_container">
